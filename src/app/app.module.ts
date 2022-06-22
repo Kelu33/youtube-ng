37c7ae406603fcf190eff9a94fb50e13cca6f670
Lucas Baste <kelu33@gmail.com>
2022-06-22 13:48:27 +0200

    width issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { VideoCardComponent } from './video-card/video-card.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VideoCardComponent
+    VideoCardComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule
